8a2fbdbb340516169700276db3a72ad9154cafe1
DeveloperAyushKumar <ayush30000000000@gmail.com>
2025-01-10 23:50:35 +0530

    frontend phase 1 done

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -3,6 +3,8 @@ import App from "../App";
 import Home from "../pages/Home/Home";
 import Login from "../components/Login";
 import Register from "../components/Register";
+import CartPage from "../pages/Books/CartPage";
+import Checkout from "../pages/Books/CheckoutPage";
 const router =createBrowserRouter([
     {
         path:"/",
@@ -28,6 +30,15 @@ const router =createBrowserRouter([
             {
                 path:"/register",
                 element:<Register/>
+            },
+            {
+                path:"/cart",
+                element:<CartPage/>
+            },
+            {
+                path :"/checkout",
+                element:<Checkout/>
+
             },
             
         ]
